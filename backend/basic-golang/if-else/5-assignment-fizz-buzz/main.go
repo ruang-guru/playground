9c2522357a3de402ab06ac77450dca5bef2a506c
Febri Hidayan <febrihidayan20@gmail.com>
2022-03-17 13:56:24 +0700

    wip

--- backend/basic-golang/if-else/5-assignment-fizz-buzz/main.go
+++ backend/basic-golang/if-else/5-assignment-fizz-buzz/main.go
@@ -17,10 +17,12 @@ func main() {
 		// TODO: answer here
 		if (i % 3) == 0 && (i %5) == 0 {
 			fmt.Println("fizzbuzz")
+		} else if ((i % 3) == 0) {
+			fmt.Println("fizz")
 		} else if ((i % 5) == 0) {
 			fmt.Println("buzz")
 		} else {
-			fmt.Println("fizz")
+			fmt.Println(i)
 		}
 	}
 }

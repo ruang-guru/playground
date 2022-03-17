687c41ffd36adb55c1f52171814fcdaa15fab949
Febri Hidayan <febrihidayan20@gmail.com>
2022-03-17 13:52:00 +0700

    answer 5-assignment-fizz-buzz

--- backend/basic-golang/if-else/5-assignment-fizz-buzz/main.go
+++ backend/basic-golang/if-else/5-assignment-fizz-buzz/main.go
@@ -15,5 +15,12 @@ import "fmt"
 func main() {
 	for i := 1; i <= 100; i++ {
 		// TODO: answer here
+		if (i % 3) == 0 && (i %5) == 0 {
+			fmt.Println("fizzbuzz")
+		} else if ((i % 5) == 0) {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println("fizz")
+		}
 	}
 }

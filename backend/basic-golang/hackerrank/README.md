2556231fdee3852beb978d7a708ac1b734dfcf17
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2022-03-19 13:50:17 +0700

    chore: sync

--- backend/basic-golang/hackerrank/README.md
+++ backend/basic-golang/hackerrank/README.md
@@ -10,7 +10,7 @@
 - Discussion and Presentation
 
 
-## Lecture:
+## Basic:
 - Problem 1: [Compare the Triplet Problem](compare-the-triplets/main.go)
 - Problem 2: [A Very Big Sum](very-big-sum/main.go)
 - Problem 3: [Diagonal Difference](diagonal-differencer/main.go)
@@ -19,3 +19,11 @@
 - Problem 6: [Birthday Cake Candles](birthday-cake-candles/main.go)
 - Problem 7: [Number Line Jumps](number-line-jumps/main.go)
 - Problem 8: [Apple & Orrage](apple-and-orange/main.go)
+
+## Intermediate:
+- Problem 1: [Pairs](pairs/main.go)
+- Problem 2: [The time in Words](the-time-in-words/main.go)
+- Problem 3: [Encryption](encryption/main.go)
+- Problem 4: [Recursive Digit Sum](recursive-digit-sum/main.go)
+- Problem 5: [Counter Game](counter-game/main.go)
+- Problem 6: [Sherlock and Valid String](sherlock-and-valid-string/main.go)
---
+++ backend/basic-golang/hackerrank/pairs/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'pairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY arr
+ */
+
+// Problem statement: https://www.hackerrank.com/challenges/pairs/
+func pairs(k int32, arr []int32) int32 {
+	// Write your code here
+	return 0 // TODO: replace this
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var arr []int32
+
+	for i := 0; i < int(n); i++ {
+		arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+		checkError(err)
+		arrItem := int32(arrItemTemp)
+		arr = append(arr, arrItem)
+	}
+
+	result := pairs(k, arr)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}

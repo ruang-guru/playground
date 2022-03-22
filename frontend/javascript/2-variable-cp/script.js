886301a0b4b9e8340e1924e9d20bfef63674f431
Riski Adi Pratama <83565699+riskiadi16@users.noreply.github.com>
2022-03-22 15:10:46 +0700

    Update script.js
    Answer

--- frontend/javascript/2-variable-cp/script.js
+++ frontend/javascript/2-variable-cp/script.js
@@ -4,6 +4,9 @@ let a = prompt('Masukan variabel pertama: ');
 let b = prompt('Masukan variabel kedua: ');
 
 // TODO: answer here
+a = a + b;
+b = a - b;
+a = a - b;
 
 console.log(`Nilai a setelah ditukar: ${a}`);
 console.log(`Nilai b setelah ditukar: ${b}`);

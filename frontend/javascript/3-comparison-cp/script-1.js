9253370177dde7f67f8e970a1096ab538a00faf8
Riski Adi Pratama <83565699+riskiadi16@users.noreply.github.com>
2022-03-22 15:31:20 +0700

    Pengecekan angka positif, negatif, atau 0
    Answer Pengecekan angka positif, negatif, atau 0 dengan nested if

--- frontend/javascript/3-comparison-cp/script-1.js
+++ frontend/javascript/3-comparison-cp/script-1.js
@@ -1,6 +1,19 @@
 // Pengecekan angka positif, negatif, atau 0 dengan nested if
 
 // Masukan suatu angka
-const number = parseInt(prompt("Masukan bilangan: "));
+const number = parseInt(prompt("Enter a number: "));
 
-// TODO: answer here
+// check if number is greater than 0
+if (number > 0) {
+    console.log("Angka Positif");
+}
+
+// check if number is 0
+else if (number == 0) {
+  console.log("Angka Negatif");
+}
+
+// if number is less than 0
+else {
+     console.log("Angka Negatif");
+}

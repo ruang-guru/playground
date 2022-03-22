1752dc1a5367c2f4a8d293aca0461994128b2152
Anas Ardiansyah <66627107+anmistrz@users.noreply.github.com>
2022-03-22 15:02:31 +0700

    Update script-1.js
    answering script-1

--- frontend/javascript/3-comparison-cp/script-1.js
+++ frontend/javascript/3-comparison-cp/script-1.js
@@ -4,3 +4,10 @@
 const number = parseInt(prompt("Masukan bilangan: "));
 
 // TODO: answer here
+if(number < 0 ){
+    alert("Bilangan negatif");
+}else if(number > 0){
+    alert("Bilangan positif");
+}else{
+    alert("Bilangan nol");
+}

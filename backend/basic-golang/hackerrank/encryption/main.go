79b51ea7f82ae7a59c13a3d19793cd6b213401ef
saifulAchmad <achmadfajr131101@gmail.com>
2022-03-23 14:14:45 +0700

    sc hackerrank

--- backend/basic-golang/hackerrank/encryption/main.go
+++ backend/basic-golang/hackerrank/encryption/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"bufio"
-	"crypto/rand"
 	"fmt"
 	"io"
 	"math"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -17,43 +17,65 @@ import (
  * The function accepts STRING s as parameter.
  */
 
-//Problem statement: https://www.hackerrank.com/challenges/encryption/problem
-
 func encryption(s string) string {
-	// Write your code here
-	return "" // TODO: replace this
+    // Write your code here
+
+	rows := math.Floor(math.Sqrt(float64(len(s))))
+	collumn := math.Ceil(math.Sqrt(float64(len(s))))
+	var slice []string
+	
+	for i := 0; i < len(s); i++ {
+		slice =append(slice,string(s[i]))
+	}
+
+	for{
+		for i := 0; i < int(rows); i++ {
+		temp:= rand.Intn(int(collumn))	
+		fmt.Print(s[temp])
+		
+		}
+
+		count:= 1
+		if count ==int(collumn) {
+			continue
+		}
+		if len(slice)==0 {
+			break
+		}
+	}
+
 }
 
 func main() {
-	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
 
-	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
-	checkError(err)
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
 
-	defer stdout.Close()
+    defer stdout.Close()
 
-	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
 
-	s := readLine(reader)
+    s := readLine(reader)
 
-	result := encryption(s)
+    result := encryption(s)
 
-	fmt.Fprintf(writer, "%s\n", result)
+    fmt.Fprintf(writer, "%s\n", result)
 
-	writer.Flush()
+    writer.Flush()
 }
 
 func readLine(reader *bufio.Reader) string {
-	str, _, err := reader.ReadLine()
-	if err == io.EOF {
-		return ""
-	}
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
 
-	return strings.TrimRight(string(str), "\r\n")
+    return strings.TrimRight(string(str), "\r\n")
 }
 
 func checkError(err error) {
-	if err != nil {
-		panic(err)
-	}
+    if err != nil {
+        panic(err)
+    }
 }

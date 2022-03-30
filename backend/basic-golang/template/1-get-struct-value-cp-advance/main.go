93307a2b4def546d392fdac8b614687d072f7f05
Yoga Cahya R <yogacahya08@gmail.com>
2022-03-30 21:11:42 +0700

    added answer for 1-get-struct-value-cp-advance

--- backend/basic-golang/template/1-get-struct-value-cp-advance/main.go
+++ backend/basic-golang/template/1-get-struct-value-cp-advance/main.go
@@ -17,11 +17,23 @@ type Student struct {
 
 func (s Student) CalculateScore(scores []float64) float64 {
 	// TODO: answer here
+	var total float64
+	for _, score := range scores {
+		total += score
+	}
+	return total / float64(len(scores))
 }
 
 func (s Student) GenerateStudentTemplate() string {
 	buff := new(bytes.Buffer)
 	// TODO: answer here
+	tmp1, err := template.New("template_1").Parse("Hello {{.Name}}, Nilai rata-rata kamu {{.CalculateScore .Scores}}")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmp1.Execute(buff, s)
+
 	return buff.String()
 }
 

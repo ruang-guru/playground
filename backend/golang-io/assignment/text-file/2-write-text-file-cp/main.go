b749f5399f3138c22d44021c84ef8c92d50d042c
Yoga Cahya R <yogacahya08@gmail.com>
2022-04-04 16:20:03 +0700

    added answer for 2-write-text-file-cp

--- backend/golang-io/assignment/text-file/2-write-text-file-cp/main.go
+++ backend/golang-io/assignment/text-file/2-write-text-file-cp/main.go
@@ -8,5 +8,15 @@ import (
 // dalam test ini terdapat fungsi os.Remove ya. itu automatis nge remove file yang telah dibuat
 // Untuk keperluan testing
 func WriteFile(fileName string, fileData string) error {
-	return nil // TODO: replace this
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	_, err = file.WriteString(fileData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return err // TODO: replace this
 }

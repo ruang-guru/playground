4e71c81cedea297357065812179043db5bda3519
Yoga Cahya R <yogacahya08@gmail.com>
2022-04-12 16:35:15 +0700

    added answer for stack cp

--- backend/data-structure/stack/1-init-cp/main.go
+++ backend/data-structure/stack/1-init-cp/main.go
@@ -5,8 +5,17 @@ package main
 // Lengkapi function NewStack sehingga function tersebut dapat mengembalikan objek Stack dengan memiliki ukuran dari parameter.
 type Stack struct {
 	// TODO: answer here
+	Size int
+	Top  int
+	Data []int
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+	s := Stack{}
+	s.Size = size
+	s.Top = -1
+	s.Data = make([]int, 0)
+
+	return s
 }
--- backend/data-structure/stack/2-push-cp/main.go
+++ backend/data-structure/stack/2-push-cp/main.go
@@ -10,12 +10,29 @@ var ErrStackOverflow = errors.New("stack overflow")
 
 type Stack struct {
 	// TODO: answer here
+	Size int
+	Top  int
+	Data []int
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+
+	s := Stack{}
+	s.Data = make([]int, 0)
+	s.Size = size
+	s.Top = -1
+
+	return s
 }
 
 func (s *Stack) Push(Elemen int) error {
 	// TODO: answer here
+	if s.Top+1 >= s.Size {
+		return ErrStackOverflow
+	}
+
+	s.Top++
+	s.Data = append(s.Data, Elemen)
+	return nil
 }

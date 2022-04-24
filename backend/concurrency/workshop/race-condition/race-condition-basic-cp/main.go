8f09661a17031f72f58eb48335912e1b0edeed53
Yoga Cahya R <yogacahya08@gmail.com>
2022-04-24 20:31:32 +0700

    added answer for race condition and wait group cp

--- backend/concurrency/workshop/race-condition/race-condition-basic-cp/main.go
+++ backend/concurrency/workshop/race-condition/race-condition-basic-cp/main.go
@@ -6,7 +6,7 @@ import "sync"
 func counter(output chan<- int) {
 
 	// TODO: answer here
-
+	ch := make(chan int)
 	var wg sync.WaitGroup
 	count := 0
 	for i := 0; i < 1000; i++ {
@@ -14,11 +14,18 @@ func counter(output chan<- int) {
 		go func() {
 			//kirim 1 ke channel
 			// TODO: answer here
+			defer wg.Done()
+			ch <- 1
 		}()
 	}
 	//mengubah nilai count menggunakan data dari channel
 
 	// TODO: answer here
+	go func() {
+		for {
+			count += <-ch
+		}
+	}()
 	wg.Wait() // menunggu seluruh goroutine selesai berjalan
 	output <- count
 }

4a193158b31cc93ec2f6393063efa09dc41a01d9
Yoga Cahya R <yogacahya08@gmail.com>
2022-05-10 00:49:55 +0700

    Added answer for vegeta CP and increase attack + spike test

--- backend/performance-testing/assignment/increase-attack-cp/main.go
+++ backend/performance-testing/assignment/increase-attack-cp/main.go
@@ -17,8 +17,21 @@ frequency := 1
 increaseValue := 2
 */
 func increaseTest(target string) *vegeta.Metrics {
-	metrics := &vegeta.Metrics{}
 	// TODO: answer here
+	duration := 4 * time.Second
+	frequency := 1
+	increaseValue := 2
+
+	rate := vegeta.Rate{
+		Per: time.Second, Freq: frequency,
+	}
+
+	targeter := vegeta.NewStaticTargeter(vegeta.Target{
+		Method: "GET",
+		URL:    target,
+	})
+
+	metrics := vegetaAttackIncreaseBySecond(targeter, rate, duration, increaseValue)
 	return metrics
 }
 
@@ -33,6 +46,15 @@ func increaseTest(target string) *vegeta.Metrics {
 func vegetaAttackIncreaseBySecond(targeter vegeta.Targeter, rate vegeta.ConstantPacer, duration time.Duration, increaseValue int) *vegeta.Metrics {
 	var metrics vegeta.Metrics
 	// TODO: answer here
+	attacker := vegeta.NewAttacker()
+
+	for start := time.Now(); time.Since(start) < duration; {
+		for res := range attacker.Attack(targeter, rate, time.Second, "attack") {
+			metrics.Add(res)
+		}
+		rate.Freq += increaseValue
+	}
+
 	metrics.Close()
 	return &metrics
 }
